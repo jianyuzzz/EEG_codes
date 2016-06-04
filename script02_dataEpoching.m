54bd749d82a174e56a321ba6e2d8e6d7a8f27a50
JianyuZhao93 <planetzjy@gmail.com>
2016-06-04 11:24:31 +0200

    Initialization
    Ran the EEG codes for the first time

---
+++ script02_dataEpoching.m
@@ -0,0 +1,100 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%
+% Data analysis of dataset SPUELER2015
+% Epoch extraction
+%
+% Input:    EEGLAB like data format 
+% Output:   struct file containing 3 types of epoched data, no error, 
+%           execution error, outcome error
+%
+% Author: Stefan Ehrlich
+% Modificated by Jianyu Zhao
+% Last revised: 03.06.2016
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all
+
+inpath = strcat(pwd,'/data_pp/');
+outpath = strcat(pwd,'/data_epo/');
+
+% List of filenames of all subjects
+% subjects = {'S01','S02','S03','S04','S05','S06','S07','S08','S09','S10'};
+subjects = {'S01'};
+
+for s = 1:length(subjects)
+    
+    filename = strcat(subjects{s},'.set');% e.g. S1.set
+    out_filename = strcat(subjects{s})% File name for output: S1
+    EEG = pop_loadset(strcat(inpath,filename))
+    % Load an EEG dataset, named as simple as EEG
+
+    %% copy necessary information
+    EPO.srate = EEG.srate;% sampling rate
+    EPO.chanlocs = EEG.chanlocs;% channel location information (10-10,10-20)
+    
+    %% extract trials
+    
+    [EEG_epo] = pop_epoch(EEG,{'1'},[0 1])
+    % convert a continuous EEG dataset to epoched data,
+    % by extracting data epochs time locked to specified event types
+    EPO.exec = EEG_epo.data;
+    [EEG_epo] = pop_epoch(EEG,{'2'},[0 1])% (EEG, events, timelimits)
+    EPO.out = EEG_epo.data;% epoched execution error and outcome error data
+    
+    % recover latencies (????)
+    % record all the moments when errors happend
+    execErr = [];
+    outErr = [];
+    for e = 1:length(EEG.event)
+        if EEG.event(e).type == '1'
+            execErr = cat(2,execErr,EEG.event(e).latency);
+            % {'1'} means there is execution error
+            % add the latencies of execution error to the list
+        end
+        if EEG.event(e).type == '2'
+            outErr = cat(2,outErr,EEG.event(e).latency);
+            % {'2'}: outcome error
+        end
+    end
+    
+    EPO.times = 0:1/EEG.srate:1-1/EEG.srate;
+    % a grid from 0 to 1 - 1/EEG.srate; 1/srate means normalized freq.
+
+
+    % extract noError trials by sliding window
+    windowSize = EEG.srate;
+    windowShift = EEG.srate;
+    errors = cat(2,execErr,outErr);% concatenate the error moments
+    c = 1;
+    for trial = 1:size(EEG.data,2)/(windowShift)-windowSize
+        %## exclude samples near execution and outcome errors
+        errortrial = 0;
+        for i = 1:length(errors)
+            if ((trial-1)*windowShift+windowSize/2 >= errors(i)-EEG.srate) &&...
+                ((trial-1)*windowShift+windowSize/2 <= errors(i)+EEG.srate*3)
+                errortrial = 1;
+            else
+                continue;
+            end
+        end
+    
+        if errortrial == 0
+            EPO.noError(:,:,c) = EEG.data(:,(trial-1)*windowShift+1:(trial-1)*windowShift+windowSize);
+            c = c+1;
+        end
+    end
+
+    figure(s)% 3 lines of means of no error, execution error and outcome error epochs
+    plot(EPO.times,mean(EPO.noError(8,:,:),3))
+    hold on
+    plot(EPO.times,mean(EPO.exec(8,:,:),3),'g')
+    plot(EPO.times,mean(EPO.out(8,:,:),3),'r')
+    
+    eeglab2loreta(EPO.chanlocs, mean(EPO.exec(:,:,:), 3), 'exporterp', 'on');
+
+save(strcat(outpath,out_filename),'EPO')
+
+end
---
+++ script03_featureExtraction.m
@@ -0,0 +1,103 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%
+% Data analysis of dataset SPUELER2015
+% Feature extraction
+% 
+% Input:    struct file containing 3 types of epoched data, no error, 
+%           execution error, outcome error 
+% Output:   struct file containing different feature types and labels
+%
+% Author: Stefan Ehrlich
+% Last revised: 23.02.2016
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all
+
+inpath = strcat(pwd,'/data_epo/');% horizontal string concentenation
+outpath = strcat(pwd,'/data_feat/');% owd shows the current working directory
+
+% List of filenames of all subjects
+% subjects = {'S01','S02','S03','S04','S05','S06','S07','S08','S09','S10'};
+subjects = {'S01'};
+%s= 1;
+
+for s=1:length(subjects)
+    
+    filename = subjects{s};% e.g. 'S01'
+    %filename = strcat(subjects{s},'.mat');
+    load(strcat(inpath,filename))
+    
+    FEAT.srate = EPO.srate;
+    FEAT.chanlocs = EPO.chanlocs;% sampling rate and channel locations 
+                                 % remain the same in feature data files
+    
+    %% Cosntruct label vector
+    nTrials_noError = size(EPO.noError,3);% number of no-error trials
+    nTrials_exec = size(EPO.exec,3);
+    nTrials_out = size(EPO.out,3);
+    % the 3rd dim means the number of corresponding epochs
+    
+    FEAT.labels = [zeros(nTrials_noError,1);zeros(nTrials_exec,1)+1;zeros(nTrials_out,1)+2];
+    % 3 columns with different lengths:[0;1;2]
+    
+    %% Extraction of temporal features
+    FEAT.times = EPO.times;% same selected epochs
+    FEAT.temp = cat(3,EPO.noError,EPO.exec,EPO.out);% simply contatenate the epoched data 
+    
+    %% Extraction of frequency-domain features
+    winSize = 128;
+    noverlap = 64;
+    nfft = 256;% length of fft 
+    
+    for ch = 1:size(FEAT.temp,1)% for every channel, 28 
+        for trial = 1:size(FEAT.temp,3)% for every trial
+            [pxx(:,ch,trial) FEAT.f] = pwelch(FEAT.temp(ch,EPO.times>0.2 & EPO.times<0.9,trial),winSize,noverlap,nfft,EPO.srate);
+            % estimate the spectral density of temporary features in this
+            % case
+        end
+        fprintf(strcat(num2str(ch),'\n'))
+    end
+    FEAT.freq = permute(pxx(1:40,:,:),[2 1 3]);% rearrange the dimensions of A to 2-by-1-by-3
+    FEAT.f = FEAT.f(1:40);% only reserve the main 40 frequencies
+    clear pxx
+    
+    figure(s) % frequency-domain features of the 8th channel
+    plot(FEAT.f,mean(FEAT.freq(8,:,FEAT.labels==0),3))
+    hold on
+    plot(FEAT.f,mean(FEAT.freq(8,:,FEAT.labels==1),3),'g')
+    plot(FEAT.f,mean(FEAT.freq(8,:,FEAT.labels==2),3),'r')
+    
+    
+    %% Extraction of connectivity features
+%{
+    for trial = 1:size(FEAT.temp,3)% correlation
+        FEAT.conn(:,:,trial) = corr(FEAT.temp(:,EPO.times>0.2 & EPO.times<0.9,trial)');
+    end
+%}    
+
+
+    for trial = 1:size(FEAT.temp,3) % covariance
+       FEAT.conn2(:,:,trial) = cov(FEAT.temp(:,:,trial)')-(trace(cov(FEAT.temp(:,:,trial)')))/28;
+        %FEAT.conn2(:,:,trial) = cov(FEAT.temp(:,:,trial)');
+    end    
+    subplot(2,2,1)
+    imagesc(mean(FEAT.conn2(:,:,FEAT.labels==1),3)-mean(FEAT.conn2(:,:,FEAT.labels==0),3))
+    cbar
+    subplot(2,2,2)
+    imagesc(mean(FEAT.conn2(:,:,FEAT.labels==2),3)-mean(FEAT.conn2(:,:,FEAT.labels==0),3))
+    cbar
+    subplot(2,2,3)
+    imagesc(mean(FEAT.conn2(:,:,FEAT.labels==2),3)-mean(FEAT.conn2(:,:,FEAT.labels==1),3))
+    cbar
+
+    save(strcat(outpath,filename),'FEAT')
+    clear FEAT
+    
+end
+    
+    
+    
+    
